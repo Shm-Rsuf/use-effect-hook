aa1e809cfe853436afb37cb80e0ef6dd99e65c95
shm_usuf <shmusuf@gmail.com>
2022-11-23 15:40:54 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [isGoal, setIsGoal] = useState(false);
+
+  const incrementHandler = () => {
+    setCount((previous) => previous + 1);
+  };
+
+  useEffect(() => {
+    setIsGoal(!isGoal);
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Count: {count}</h2>
+      <h2>{isGoal ? "Goal" : "No Goal"}</h2>
+      <button onClick={incrementHandler}>increment</button>
     </div>
   );
 }
